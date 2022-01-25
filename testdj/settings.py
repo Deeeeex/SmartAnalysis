e294efe7953790a8b1cc679924fb14a1705630d4
Deeeeex <482788525@qq.com>
2022-01-25 23:10:25 +0800

    Done!

--- testdj/settings.py
+++ testdj/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'testdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'login',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
