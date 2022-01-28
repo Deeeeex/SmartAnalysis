aa22150756e68f4d3850f0c028c2257d4b47729c
Deeeeex <482788525@qq.com>
2022-01-28 22:06:59 +0800

    admin using simpleui

--- login/admin.py
+++ login/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import UserInfo
+from tasks.models import Task
 
+
+admin.site.site_header = 'TimeSeries管理后台'  # 设置header
+admin.site.site_title = 'TimeSeries管理后台'   # 设置title
+admin.site.index_title = 'TimeSeries管理后台'
 # Register your models here.
 admin.site.register(UserInfo)
+admin.site.register(Task)
+
